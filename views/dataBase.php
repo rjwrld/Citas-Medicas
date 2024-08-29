ddcf6ef32e7069239f6ca1d04f55ab3886aeced9
DEREK1799 <142855049+DEREK1799@users.noreply.github.com>
2024-08-29 11:05:07 -0600

    Update dataBase.php

--- views/dataBase.php
+++ views/dataBase.php
@@ -11,6 +11,16 @@ if($conn->connect_error){
     die("Conexion fallida: ".$conn->connect_error);
    
 }
+
+CREATE TABLE pagos (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre_paciente VARCHAR(255) NOT NULL,
+    fecha_pago DATE NOT NULL,
+    monto DECIMAL(10, 2) NOT NULL,
+    metodo_pago ENUM('Efectivo', 'Tarjeta de Crédito', 'Tarjeta de Débito', 'Transferencia Bancaria') NOT NULL,
+    descripcion TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
  
 ?>
 
